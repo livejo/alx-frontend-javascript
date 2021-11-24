8d3cc255884ce4dbc0fdf1fbdb81e6ef7c57f0c5
livejo <yoseph.kefale@gmail.com>
2021-11-24 17:48:30 +0300

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,12 @@
-export default function getFullResponseFromAPI(success) {
-  const promise = new Promise((resolve, reject) => {
-    if (success === false) {
-      reject(Error('The fake API is not working currently'));
-    } else {
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
       resolve({
         status: 200,
-        body: 'Success',
+        body: 'success',
       });
+    } else {
+      reject(Error('The fake API is not working currently'));
     }
   });
-  return promise;
 }
