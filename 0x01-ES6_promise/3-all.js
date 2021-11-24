e83b3bcf8b51c7c4052058d5fb7429347c5f0413
livejo <yoseph.kefale@gmail.com>
2021-11-24 17:51:52 +0300

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,11 @@
-import createUser from utils
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-    createUser.then("hi")
-    console.log('hi')
+export default function handleSignUp() {
+  Promise.all([uploadPhoto(), createUser()]).then(
+    (results) => {
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+    }, () => {
+      console.log('Signup system offline');
+    },
+  );
 }
