91669a5a8fe0d91174c5eb9600134c54bb274bdb
livejo <yoseph.kefale@gmail.com>
2021-11-30 12:00:51 +0300

    Update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,3 @@
-export default function hasValuesFromArray(set, array) {
-  if (!set || !Array.isArray(array)) throw new Error('invalid parameter');
-  let output = true;
-  for (let i = 0; i < array.length; i++) {
-    set.has(array[i]) ? output = true : output = false;
-  }
-  return output;
+export default function hasValuesFromArray(set, arr) {
+  return arr.filter((x) => set.has(x)).length === arr.length;
 }
