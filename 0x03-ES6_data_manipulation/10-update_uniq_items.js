7b15e1df5745c7fa90707160e1f3e2e0a10bb631
livejo <yoseph.kefale@gmail.com>
2021-11-30 12:03:02 +0300

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,10 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-
-  for(let item of map) {
-    if (item[1] === 1) {
-      map.set(item[0], 100);
+  if (!(map instanceof Map)) {
+    throw Error('Cannot process');
+  }
+  for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
     }
   }
   return map;
